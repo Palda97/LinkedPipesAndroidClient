bf97c2e8e9b457edfb822c49db9c8ae4ed9a67c3
David Paleček <palecda1@fit.cvut.cz>
2020-08-13 04:48:55 +0200

    add floating button listener and small function reformat

--- app/src/main/java/cz/palda97/lpclient/view/settings/SettingsFragment.kt
+++ app/src/main/java/cz/palda97/lpclient/view/settings/SettingsFragment.kt
@@ -5,13 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import cz.palda97.lpclient.R
 import cz.palda97.lpclient.databinding.FragmentSettingsBinding
+import cz.palda97.lpclient.model.ServerInstance
 import cz.palda97.lpclient.viewmodel.SettingsViewModel
 
 class SettingsFragment : Fragment() {
@@ -19,6 +20,7 @@ class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
     private lateinit var viewModel: SettingsViewModel
     private lateinit var serverRecyclerAdapter: ServerRecyclerAdapter
+    private lateinit var fab: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,7 +31,6 @@ class SettingsFragment : Fragment() {
         val root = binding.root
         viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         setUpComponents()
-        setUpObservers()
         return root
     }
 
@@ -40,28 +41,34 @@ class SettingsFragment : Fragment() {
                 viewModel.notifications = isChecked
             }
         }
+
         fun setUpServerRecycler() {
             serverRecyclerAdapter = ServerRecyclerAdapter(
-                {viewModel.editServer(it)},
-                {viewModel.deleteServer(it)}
+                { viewModel.editServer(it) },
+                { viewModel.deleteServer(it) }
             )
             binding.insertServerInstancesHere.adapter = serverRecyclerAdapter
+            viewModel.liveServers.observe(viewLifecycleOwner, Observer {
+                if (it == null)
+                    return@Observer
+                if (it.isOk) {
+                    Log.d(TAG, "it.isOk")
+                    serverRecyclerAdapter.updateServerList(it.mailContent!!)
+                }
+                binding.mail = it
+                binding.executePendingBindings()
+            })
+        }
+
+        fun setUpFAB() {
+            fab = binding.fab
+            fab.setOnClickListener {
+                viewModel.editServer(ServerInstance())
+            }
         }
         setUpNotificationSwitch()
         setUpServerRecycler()
-    }
-
-    private fun setUpObservers() {
-        viewModel.liveServers.observe(viewLifecycleOwner, Observer {
-            if(it == null)
-                return@Observer
-            if(it.isOk) {
-                Log.d(TAG, "it.isOk")
-                serverRecyclerAdapter.updateServerList(it.mailContent!!)
-            }
-            binding.mail = it
-            binding.executePendingBindings()
-        })
+        setUpFAB()
     }
 
     companion object {
