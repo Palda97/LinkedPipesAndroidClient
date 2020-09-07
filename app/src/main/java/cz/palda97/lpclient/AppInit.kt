9ccd30f7fcb2d87ccd6003e9705d67b72e33a7df
David Paleček <palecda1@fit.cvut.cz>
2020-09-07 14:54:58 +0200

    fix the default broken ssl library
    it has costed me way more time that it should

--- app/src/main/java/cz/palda97/lpclient/AppInit.kt
+++ app/src/main/java/cz/palda97/lpclient/AppInit.kt
@@ -1,10 +1,20 @@
 package cz.palda97.lpclient
 
 import android.app.Application
+import android.content.Context
+import org.conscrypt.Conscrypt
+import java.security.Security
 
-class AppInit: Application() {
+class AppInit : Application() {
     override fun onCreate() {
         super.onCreate()
-        Injector.context = applicationContext
+        init(applicationContext)
+    }
+
+    companion object {
+        fun init(context: Context) {
+            Injector.context = context
+            Security.insertProviderAt(Conscrypt.newProvider(), 1)
+        }
     }
 }
