c21ceee5dd64df64799d16fbdd54452e29626f57
David Paleček <palecda1@fit.cvut.cz>
2020-09-24 12:02:35 +0200

    add live executions to viewmodel

--- app/src/main/java/cz/palda97/lpclient/viewmodel/executions/ExecutionsViewModel.kt
+++ app/src/main/java/cz/palda97/lpclient/viewmodel/executions/ExecutionsViewModel.kt
@@ -3,19 +3,36 @@ package cz.palda97.lpclient.viewmodel.executions
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import cz.palda97.lpclient.Injector
-import cz.palda97.lpclient.model.ServerInstance
+import cz.palda97.lpclient.model.*
 import cz.palda97.lpclient.model.repository.ExecutionRepository
 import cz.palda97.lpclient.model.repository.ServerRepository
+import java.util.*
 
-class ExecutionsViewModel(application: Application): AndroidViewModel(application) {
+class ExecutionsViewModel(application: Application) : AndroidViewModel(application) {
 
     private val executionRepository: ExecutionRepository = Injector.executionRepository
     private val serverRepository: ServerRepository = Injector.serverRepository
 
+    private val executionVList: List<ExecutionV> = listOf(
+        ExecutionV("1", "Home", "random pipeline", "1.8.2020", ExecutionStatus.FINISHED),
+        ExecutionV("2", "Work", "serious pl", "1.9.2020", ExecutionStatus.FINISHED),
+        ExecutionV("3", "Test", "haha", "15.9.2020", ExecutionStatus.RUNNING),
+        ExecutionV("4", "Home", "asdf", "9.6.2020", ExecutionStatus.FAILED),
+        ExecutionV("5", "Home", "my pipeline", "20.9.2020", ExecutionStatus.FINISHED)
+    )
+
+    private val _liveExecutions: MutableLiveData<MailPackage<List<ExecutionV>>> =
+        MutableLiveData(MailPackage(executionVList))
+    val liveExecutions: LiveData<MailPackage<List<ExecutionV>>>
+        get() = _liveExecutions
+
     private fun onServerToFilterChange() {
         //TODO
     }
+
     var serverToFilter: ServerInstance?
         get() = serverRepository.serverToFilter
         private set(value) {
@@ -25,6 +42,7 @@ class ExecutionsViewModel(application: Application): AndroidViewModel(applicatio
                 onServerToFilterChange()
             }
         }
+
     fun setServerToFilterFun(serverInstance: ServerInstance?) {
         serverToFilter = serverInstance
     }
