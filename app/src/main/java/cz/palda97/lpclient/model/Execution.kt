bb614816f8c0c2a1f14722608aa93f92a7172336
David Paleček <palecda1@fit.cvut.cz>
2020-09-24 11:56:35 +0200

    override equals and hashCode in execution

--- app/src/main/java/cz/palda97/lpclient/model/Execution.kt
+++ app/src/main/java/cz/palda97/lpclient/model/Execution.kt
@@ -29,6 +29,23 @@ data class Execution(
 
     val idNumber: String
         get() = id.split("/").last()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Execution
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+
 }
 
 enum class ExecutionStatus {
