83b41b4422b4c853067a574b4d3b1b67879dbd21
David Paleček <palecda1@fit.cvut.cz>
2020-09-25 20:33:59 +0200

    add some methods to execution dao

--- app/src/main/java/cz/palda97/lpclient/model/db/dao/ExecutionDao.kt
+++ app/src/main/java/cz/palda97/lpclient/model/db/dao/ExecutionDao.kt
@@ -1,8 +1,6 @@
 package cz.palda97.lpclient.model.db.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.*
 import cz.palda97.lpclient.model.entities.execution.Execution
 
 @Dao
@@ -13,4 +11,19 @@ abstract class ExecutionDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract suspend fun insert(list: Execution)
+
+    @Delete
+    abstract suspend fun delete(execution: Execution)
+
+    @Query("delete from execution")
+    abstract suspend fun deleteAll()
+
+    @Transaction
+    open suspend fun renewal(list: List<Execution>) {
+        deleteAll()
+        insert(list)
+    }
+
+    @Query("select * from execution where deleted = 1")
+    abstract suspend fun selectDeleted(): List<Execution>
 }
