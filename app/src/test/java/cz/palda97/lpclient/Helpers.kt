5382ffcf9f579eee94ed650782c4a586af1f9327
David Paleček <palecda1@fit.cvut.cz>
2020-09-29 19:48:07 +0200

    add sleep function to test helper class

--- app/src/test/java/cz/palda97/lpclient/Helpers.kt
+++ app/src/test/java/cz/palda97/lpclient/Helpers.kt
@@ -1,5 +1,12 @@
 package cz.palda97.lpclient
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
 fun String.println() {
     println(this)
+}
+
+fun sleep(timeMillis: Long) = runBlocking {
+    delay(timeMillis)
 }
