27724c9ada7963a53d9971fd94895e22a6f0d007
David Paleček <palecda1@fit.cvut.cz>
2020-10-04 22:01:59 +0200

    remove abstract server repository

--- app/src/main/java/cz/palda97/lpclient/model/repository/ServerRepository.kt
+++ app/src/main/java/cz/palda97/lpclient/model/repository/ServerRepository.kt
@@ -2,20 +2,51 @@ package cz.palda97.lpclient.model.repository
 
 import androidx.lifecycle.LiveData
 import cz.palda97.lpclient.model.MailPackage
+import cz.palda97.lpclient.model.db.dao.ServerInstanceDao
 import cz.palda97.lpclient.model.entities.server.ServerInstance
+import androidx.lifecycle.Transformations
 
-abstract class ServerRepository {
-    abstract suspend fun insertServer(serverInstance: ServerInstance)
+class ServerRepository(private val serverInstanceDao: ServerInstanceDao) {
+
+    suspend fun insertServer(serverInstance: ServerInstance) {
+        serverInstanceDao.insertServer(serverInstance)
+    }
+
+    suspend fun matchUrlOrNameExcept(
+        serverInstance: ServerInstance,
+        except: ServerInstance
+    ): MatchCases {
+        val list = serverInstanceDao.matchExcept(serverInstance.url, serverInstance.name, except.url, except.name)
+        if (list.isEmpty())
+            return MatchCases.NO_MATCH
+        return if (list.first().url == serverInstance.url) MatchCases.URL else MatchCases.NAME
+    }
+
+    val liveServers: LiveData<MailPackage<List<ServerInstance>>> = Transformations.map(serverInstanceDao.serverList()) {
+        if (it == null)
+            return@map MailPackage.loadingPackage<List<ServerInstance>>()
+        return@map MailPackage(it)
+    }
+    val activeLiveServers: LiveData<MailPackage<List<ServerInstance>>> = Transformations.map(serverInstanceDao.activeServersOnly()) {
+        if (it == null)
+            return@map MailPackage.loadingPackage<List<ServerInstance>>()
+        return@map MailPackage(it)
+    }
+
+    suspend fun deleteAll() {
+        serverInstanceDao.deleteAll()
+    }
+
+    suspend fun deleteServer(serverInstance: ServerInstance) {
+        serverInstanceDao.deleteServer(serverInstance)
+    }
+
+    var serverToFilter: ServerInstance? = null
+
     enum class MatchCases {
         NO_MATCH, URL, NAME
     }
-    abstract suspend fun matchUrlOrNameExcept(serverInstance: ServerInstance, except: ServerInstance): MatchCases
-    suspend fun matchUrlOrName(serverInstance: ServerInstance) = matchUrlOrNameExcept(serverInstance,
-        ServerInstance()
-    )
-    abstract val liveServers: LiveData<MailPackage<List<ServerInstance>>>
-    abstract val activeLiveServers: LiveData<MailPackage<List<ServerInstance>>>
-    abstract suspend fun deleteAll()
-    abstract suspend fun deleteServer(serverInstance: ServerInstance)
-    abstract var serverToFilter: ServerInstance?
+
+    suspend fun matchUrlOrName(serverInstance: ServerInstance) =
+        matchUrlOrNameExcept(serverInstance, ServerInstance())
 }
