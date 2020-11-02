accb87ca1375a2af2cb2fa087f5841bed49a6804
David Paleček <palecda1@fit.cvut.cz>
2020-11-02 18:41:37 +0100

    setup dropdown in create pipeline dialog

--- app/src/main/java/cz/palda97/lpclient/view/editpipeline/CreatePipelineDialog.kt
+++ app/src/main/java/cz/palda97/lpclient/view/editpipeline/CreatePipelineDialog.kt
@@ -9,21 +9,42 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import cz.palda97.lpclient.Injector
 import cz.palda97.lpclient.R
 import cz.palda97.lpclient.databinding.DialogChooseServerBinding
+import cz.palda97.lpclient.view.EditPipelineActivity
+import cz.palda97.lpclient.view.ServerDropDownMagic.setUpWithServers
+import cz.palda97.lpclient.viewmodel.CommonViewModel
+import cz.palda97.lpclient.viewmodel.settings.SettingsViewModel
 
-class CreatePipelineDialog: DialogFragment() {
+class CreatePipelineDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val binding: DialogChooseServerBinding = DataBindingUtil.inflate(layoutInflater, R.layout.dialog_choose_server, null, false)
+
+        val binding: DialogChooseServerBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.dialog_choose_server, null, false)
+        val commonViewModel = CommonViewModel.getInstance(this)
+        val settingsViewModel = SettingsViewModel.getInstance(this)
+
+        binding.serverInstanceDropDown.setUpWithServers(
+            requireContext(),
+            settingsViewModel,
+            this,
+            { commonViewModel.setServerToFilterFun(it) },
+            commonViewModel.serverToFilter,
+            false
+        )
+
         val builder = MaterialAlertDialogBuilder(requireContext())
             .setView(binding.root)
             .setTitle(R.string.choose_a_server_instance)
-            .setNeutralButton(R.string.cancel) {_, _ ->
+            .setNeutralButton(R.string.cancel) { _, _ ->
                 //
             }
-            .setPositiveButton(R.string.continue_string) {_, _ ->
-                //
+            .setPositiveButton(R.string.continue_string) { _, _ ->
+                if (commonViewModel.serverToFilter == null) {
+                    //Maybe show some toast or something
+                    return@setPositiveButton
+                }
+                EditPipelineActivity.start(requireContext())
             }
-
 
         return builder.create()
     }
