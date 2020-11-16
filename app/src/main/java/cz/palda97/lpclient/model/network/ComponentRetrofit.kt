98053b862a09507e8efd3f3f15315808843f46b0
David Paleček <palecda1@fit.cvut.cz>
2020-11-16 16:51:33 +0100

    add component retrofit

---
+++ app/src/main/java/cz/palda97/lpclient/model/network/ComponentRetrofit.kt
@@ -0,0 +1,19 @@
+package cz.palda97.lpclient.model.network
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ComponentRetrofit {
+
+    @GET("api/v1/components/dialog?name=config&file=dialog.html")
+    fun dialog(@Query("iri") iri: String): Call<ResponseBody>
+
+    companion object {
+        val Retrofit.Builder.componentRetrofit: ComponentRetrofit
+            get() = build().create(ComponentRetrofit::class.java)
+    }
+}
