7aa3835e889693f1694e3dfce467418d8cd4a563
David Paleček <palecda1@fit.cvut.cz>
2021-01-22 14:55:22 +0100

    introduce generics to smart adapter

--- app/src/main/java/cz/palda97/lpclient/view/editcomponent/ConfigDropdownMagic.kt
+++ app/src/main/java/cz/palda97/lpclient/view/editcomponent/ConfigDropdownMagic.kt
@@ -6,20 +6,23 @@ import cz.palda97.lpclient.Injector
 import cz.palda97.lpclient.R
 
 object ConfigDropdownMagic {
-    fun MaterialAutoCompleteTextView.fillWithOptions(context: Context, options: List<Pair<String, String>>, onItemClick: (position: Int) -> Unit = {}) {
-        val adapter = SmartArrayAdapter(context, R.layout.dropdown_item_text_view)
-        adapter.setItems(options)
+    fun <T> MaterialAutoCompleteTextView.fillWithOptions(context: Context, options: List<Pair<T, String>>? = null, onItemClick: (position: Int) -> Unit = {}): SmartArrayAdapter<T> {
+        val adapter = SmartArrayAdapter<T>(context, R.layout.dropdown_item_text_view)
+        options?.let {
+            adapter.setItems(it)
+        }
         setAdapter(adapter)
         setOnItemClickListener { _, _, position, _ ->
             adapter.lastSelectedPosition = position
             onItemClick(position)
         }
+        return adapter
     }
 
-    fun MaterialAutoCompleteTextView.getLastSelected(): String? {
-        val adapter = adapter ?: return null.also { l("no adapter") }
-        val smartArrayAdapter = adapter as? SmartArrayAdapter ?: return null.also { l("is not smart array adapter") }
-        val id = smartArrayAdapter.lastSelectedItemId
+    inline fun <reified T: Any> MaterialAutoCompleteTextView.getLastSelected(): T? {
+        val adapter = adapter ?: return null
+        val smartArrayAdapter = adapter as? SmartArrayAdapter<*> ?: return null
+        val id = smartArrayAdapter.lastSelectedItemId as? T
         return id
     }
 
--- app/src/main/java/cz/palda97/lpclient/view/editcomponent/SmartArrayAdapter.kt
+++ app/src/main/java/cz/palda97/lpclient/view/editcomponent/SmartArrayAdapter.kt
@@ -3,16 +3,17 @@ package cz.palda97.lpclient.view.editcomponent
 import android.content.Context
 import android.widget.ArrayAdapter
 
-class SmartArrayAdapter(context: Context, resource: Int) :
-    ArrayAdapter<SmartArrayAdapter.PairWrapper>(context, resource) {
+class SmartArrayAdapter<T>(context: Context, resource: Int) :
+    ArrayAdapter<SmartArrayAdapter.PairWrapper<T>>(context, resource) {
 
-    class PairWrapper(val pair: Pair<String, String>) {
+    class PairWrapper<T>(val pair: Pair<T, String>) {
         override fun toString() = pair.second
     }
 
     var lastSelectedPosition: Int? = null
 
-    fun setItems(list: List<Pair<String, String>>) {
+    fun setItems(list: List<Pair<T, String>>) {
+        lastSelectedPosition = null
         clear()
         addAll(
             list.map {
@@ -22,7 +23,7 @@ class SmartArrayAdapter(context: Context, resource: Int) :
         notifyDataSetChanged()
     }
 
-    override fun getItem(position: Int): PairWrapper? {
+    override fun getItem(position: Int): PairWrapper<T>? {
         if (position != LAST_SELECTED)
             return super.getItem(position)
         return lastSelectedPosition?.let {
@@ -30,7 +31,7 @@ class SmartArrayAdapter(context: Context, resource: Int) :
         }
     }
 
-    val lastSelectedItemId: String?
+    val lastSelectedItemId: T?
         get() = getItem(LAST_SELECTED)?.pair?.first
 
     companion object {
