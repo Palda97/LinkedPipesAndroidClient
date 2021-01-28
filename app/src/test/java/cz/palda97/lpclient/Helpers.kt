e28bd8e6dbb1f40314055e78f0eadf34f37073c6
David Paleček <palecda1@fit.cvut.cz>
2021-01-28 16:52:56 +0100

    add string from file function to test helpers

--- app/src/test/java/cz/palda97/lpclient/Helpers.kt
+++ app/src/test/java/cz/palda97/lpclient/Helpers.kt
@@ -2,6 +2,7 @@ package cz.palda97.lpclient
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import java.io.File
 
 fun <T: Any?>T.println(): T {
     println(this)
@@ -10,4 +11,12 @@ fun <T: Any?>T.println(): T {
 
 fun sleep(timeMillis: Long) = runBlocking {
     delay(timeMillis)
+}
+
+fun <T: Any> T.stringFromFile(filename: String): String {
+    val classLoader = javaClass.classLoader
+    val path = classLoader!!.getResource(filename)?.file
+    assert(path != null) { "stringFromFile: Wrong filename!" }
+    val file = File(path!!)
+    return file.readText()
 }
