a5aac213a5afe37e6dd81e0f720d20ee5269cbee
David Paleček <palecda1@fit.cvut.cz>
2021-04-06 22:26:56 +0200

    add paretn test class for android tests and clean test

---
+++ app/src/androidTest/java/cz/palda97/lpclient/AndroidTest.kt
@@ -0,0 +1,10 @@
+package cz.palda97.lpclient
+
+//import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+//import org.junit.Test
+import org.junit.runner.RunWith
+//import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+abstract class AndroidTest
---
+++ app/src/androidTest/java/cz/palda97/lpclient/CleanTest.kt
@@ -0,0 +1,14 @@
+package cz.palda97.lpclient
+
+import org.junit.Test
+import org.junit.Assert.*
+import cz.palda97.lpclient.*
+
+class CleanTest
+    : AndroidTest() {
+
+    @Test
+    fun test() {
+        //
+    }
+}
