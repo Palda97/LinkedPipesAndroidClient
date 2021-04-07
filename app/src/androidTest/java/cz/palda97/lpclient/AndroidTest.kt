1af97a8517840dfc435ed7c4586c2b693339543e
David Paleček <palecda1@fit.cvut.cz>
2021-04-07 21:50:42 +0200

    add create db function to android test parent class
    and rule for testing livedata to clean android test

--- app/src/androidTest/java/cz/palda97/lpclient/AndroidTest.kt
+++ app/src/androidTest/java/cz/palda97/lpclient/AndroidTest.kt
@@ -1,10 +1,20 @@
 package cz.palda97.lpclient
 
 //import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-//import org.junit.Test
+import cz.palda97.lpclient.model.db.AppDatabase
 import org.junit.runner.RunWith
-//import org.junit.Assert.*
+import java.util.concurrent.Executors
 
 @RunWith(AndroidJUnit4::class)
-abstract class AndroidTest
+abstract class AndroidTest {
+
+    protected val newDb
+        get() = Room
+            .inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext<Context>(), AppDatabase::class.java)
+            .setTransactionExecutor(Executors.newSingleThreadExecutor())
+            .build()
+}
--- app/src/androidTest/java/cz/palda97/lpclient/CleanTest.kt
+++ app/src/androidTest/java/cz/palda97/lpclient/CleanTest.kt
@@ -1,11 +1,15 @@
 package cz.palda97.lpclient
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Test
 import org.junit.Assert.*
 import cz.palda97.lpclient.*
+import org.junit.Rule
 
 class CleanTest
     : AndroidTest() {
+
+    //@Rule @JvmField val rule = InstantTaskExecutorRule()
 
     @Test
     fun test() {
