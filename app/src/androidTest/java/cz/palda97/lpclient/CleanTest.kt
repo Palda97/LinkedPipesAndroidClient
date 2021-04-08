5d3d840d635a95d8f371a13da5b5b68199b48fc0
David Paleček <palecda1@fit.cvut.cz>
2021-04-08 22:46:34 +0200

    add parent test for working with db

--- app/src/androidTest/java/cz/palda97/lpclient/CleanTest.kt
+++ app/src/androidTest/java/cz/palda97/lpclient/CleanTest.kt
@@ -1,15 +1,14 @@
 package cz.palda97.lpclient
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Test
 import org.junit.Assert.*
 import cz.palda97.lpclient.*
-import org.junit.Rule
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
 
 class CleanTest
-    : AndroidTest() {
-
-    //@Rule @JvmField val rule = InstantTaskExecutorRule()
+    : TestWithDb() {
 
     @Test
     fun test() {
